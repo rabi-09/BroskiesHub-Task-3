99a1fa16bd4f4f833f2975a768de7e2aa9ca9a82
rabi.09 <dasrabi2004@gmail.com>
2025-08-15 15:35:14 +0530

    Task 3 Updated

--- NameComparator.java
+++ NameComparator.java
@@ -1,10 +1,14 @@
 import java.util.Comparator;
 
-public class NameComparator implements Comparator<Employee>{
+public class NameComparator implements Comparator{
 
     @Override
-    public int compare(Employee e1, Employee e2) {
-       return e1.getName().compareTo(e2.getName());
+    public int compare(Object o1, Object o2) {
+        Employee e1=null, e2=null;
+        if(o1 instanceof Employee && o2 instanceof Employee){
+            e1 = (Employee)o1;
+            e2 = (Employee)o2;
+        }
+        return e1.getName().compareTo(e2.getName());
     }
-    
 }
